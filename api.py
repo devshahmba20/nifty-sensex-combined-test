4fe5429cd74bdd23eb0e25ef31541c92fdf7c58e
Dev Shah <devshahmba20@users.noreply.github.com>
2026-09-18 22:01:54 +0530

    Allow deployed frontend CORS

--- api.py
+++ api.py
@@ -23,8 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
+        "https://nifty-sensex-dashboard-chfn.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
